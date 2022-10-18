a6f7ea8e7b6ce3469ea4389864ed74786eb7a28c
Michael C Main <deusprogrammer@gmail.com>
2022-10-17 23:55:34 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import Menu from './menu';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                <header>
+                    <Menu
+                        className="my-menu"
+                        title="Page Title"
+                        menu={{
+                            "Group 1": { 
+                                items: [
+                                    {
+                                        label: "Link 1",
+                                        to: "/uri1"
+                                    },
+                                    {
+                                        label: "Link 2",
+                                        to: "/uri2"
+                                    },
+                                    {
+                                        label: "Link 3",
+                                        to: "/uri3"
+                                    },
+                                    {
+                                        label: "Link 4",
+                                        to: "/uri4"
+                                    }
+                                ],
+                                show: true
+                            },
+                            "Group 2": {
+                                items: [
+                                    {
+                                        label: "Link 1",
+                                        to: "/uri1"
+                                    },
+                                    {
+                                        label: "Link 2",
+                                        to: "/uri2"
+                                    },
+                                    {
+                                        label: "Link 3",
+                                        to: "/uri3"
+                                    }
+                                ],
+                                show: false
+                            }
+                        }}/>
+                </header>
+            </div>
+        </Router>
+     );
 }
 
 export default App;
